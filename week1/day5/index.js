187936a216cee64374319669c59bb48981147e6a
jatinpatidar-tech <jatin.patidar@startappss.com>
2025-12-23 18:41:46 +0530

    counterWebApp

---
+++ week1/day5/index.js
@@ -0,0 +1,32 @@
+// // let user= new String("Hello World");
+// // console.log(typeof user); // object
+// // console.log(user.valueOf()); // Hello World
+// // console.log(user[8]); // Hello World
+// // console.log(user.toUpperCase()); // HELLO WORLD
+
+
+// // split
+// let str='my-name-is-John-Doe';
+// let arr=str.split('name');
+// console.log(arr); // [ 'my', 'name', 'is', 'John', 'Doe' ]
+// console.log(str); // [ 'my', 'name', 'is', 'John', 'Doe' ]
+
+// // join
+// let mydate= new Date(2004,4,20)
+// console.log(mydate); // 2024-06-12T10:20:30.456Z
+// // console.log(mydate); // 2024-06-12T10:20:30.456Z
+// console.log(mydate.toDateString()); // 2024-06-12 10:20:30.456Z
+
+
+let arr=[10,20,30,40,50];
+// arr.push(60);
+// console.log(arr);
+// arr.pop();  
+
+// let arr2=new Array(10,20,30,40,50);
+let arr3=arr;
+console.log(arr3);
+arr3.push(100);
+console.log(arr3);
+console.log(arr);
+
---
+++ week1/day5/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+6Z
+// console.log(mydate.toDateString()); // 2024-06-12 10:20:30.456Z
